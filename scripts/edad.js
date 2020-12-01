cc263b3d38b19dfe5d39ce73debae3ecfb754ae6
Jlcruceras <59416429+siniestr0@users.noreply.github.com>
2020-12-01 13:04:37 +0100

    cambios

--- scripts/edad.js
+++ scripts/edad.js
@@ -1,22 +1,23 @@
-    var edad = prompt("¿Cuántos años tienes?");
-    
-    if (edad > 60 ) {
-    
-        document.write("Soy un Jubilado");
-    
-    }else if (edad >= 27 & edad <=60) {
-        
-        document.write("Soy un Adulto");
-    
-    }else if (edad >= 13 & edad <=26){
-    
-        document.write("Soy un Joven");
-    
-    }else if (edad >= 0 & edad <=12){
-    
-        document.write("Soy un Niño");
-    
-    }else {
-    
-        document.write("El número no es válido.");
-    }
+//si ponemos PROMPT es para que salga la ventana
+var edad = prompt("¿Cuántos años tienes?");
+
+if (edad > 60) {
+
+    document.write("Soy un Jubilado");
+
+} else if (edad >= 27 & edad <= 60) {
+
+    document.write("Soy un Adulto");
+
+} else if (edad >= 13 & edad <= 26) {
+
+    document.write("Soy un Joven");
+
+} else if (edad >= 0 & edad <= 12) {
+
+    document.write("Soy un Niño");
+
+} else {
+
+    document.write("El número no es válido.");
+}
